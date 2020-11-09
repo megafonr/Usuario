9dd88ca94eea882bf21b6706fb130b4bb9f80a42
megafonr <70416538+megafonr@users.noreply.github.com>
2020-11-08 22:50:18 -0300

    Update Roles.java

--- src/main/java/Libre/Roles.java
+++ src/main/java/Libre/Roles.java
@@ -9,7 +9,33 @@ package Libre;
  *
  * @author Rolo
  */
+/**   Dejo Original
 public enum Roles {
     Administrador,
     Usuario
+}
+*/ 
+
+public enum Roles {
+    USR("1"),
+    ADMIN("2");
+    
+    private String itemlista;
+    
+    private Roles(String itemlista){
+        this.itemlista=itemlista;
+        
+    }
+      @Override
+    public String toString(){
+           return itemlista.trim() ;
+/**
+           if (itemlista=="2"){
+           return "2" ;
+         }
+         else{
+             return "1" ;
+         }
+*/
+    }   
 }
